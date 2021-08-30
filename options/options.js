c263e9e021a928bc37f05b80eb5362a914894560
Crowdbotics <team@crowdbotics.com>
2021-08-30 16:49:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hidden_lab_30202Identifier",
+  url: "https://hidden_lab_30202Identifier.botics.co",
+  api: "https://hidden_lab_30202Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
